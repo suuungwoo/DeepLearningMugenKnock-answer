c426f8d4165fd56a39b763fbc19a26fc9b9e4205
suuungwoo <suuungwoo@gmail.com>
2019-10-30 21:19:48 +0900

    update bias

---
+++ perceptron_sigmoid_bias.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+xs = np.array(((0, 0), (0, 1), (1, 0), (1, 1)), dtype=np.float32)
+ts = np.array((0, 0, 0, 1), dtype=np.float32)
+
+np.random.seed(0)
+w = np.random.normal(0., 1, (2))
+b = np.random.normal(0., 1, (1))
+print("weight >>", w)
+print("bias >>", b)
+
+z1 = xs
+lr = .1
+ite = 0
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+for ite in range(5000):
+    ite += 1
+    ys = sigmoid(np.dot(z1, w) + b)
+    # print("iteration:", ite, "y >>", ys)
+
+    En = -(ts - ys) * ys * (1 - ys)
+    grad_w = np.dot(z1.T, En)
+    grad_b = np.dot(np.ones([En.shape[0]]), En)
+    w -= lr * grad_w
+    b -= lr * grad_b
+
+print("training finished!")
+print("weight >>", w)
+print("bias >>", b)
+
+for x in xs:
+    ys = sigmoid(np.dot(x, w) + b)[0]
+    print("in >>", x, "out >>", ys)
